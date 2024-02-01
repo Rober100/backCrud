09daa259d0b4a93b9ef4bbd492ffc60296ec4356
Rober <roberdz30@gmail.com>
2024-02-01 11:42:39 -0300

    fourth commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ const { Router } = require("express");
 
 const router = Router()
 
-const { createNoteHandler, getAllNotesHandler, getNotesByIdHandler, deleteNoteHandler } = require("../handlers/notesHandlers");
+const { createNoteHandler, getAllNotesHandler, getNotesByIdHandler, deleteNoteHandler, updateNote } = require("../handlers/notesHandlers");
 //const  {createUserHandler}  = require("../handlers/createUserHandler")
 
 router.get("/", getAllNotesHandler)
 router.post("/noteCreate", createNoteHandler)
 router.get("/getById/:id", getNotesByIdHandler)
 router.delete("/deleteNote/:id", deleteNoteHandler)
+router.put("/update", updateNote)
 
 //router.post("/userCreate", createUserHandler);
 
